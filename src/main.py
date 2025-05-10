7a887115f8fe33ebfa0d4840ffff9a487a7d28e6
TuLe <tuleaibk@gmail.com>
2025-05-10 19:02:52 +0700

    code tournament

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import os
 import time
-from  datetime import datetime
-
-import caro
 from app import App
 from minimax import MiniMax
 from evaluate import evaluate
+import caro
+import tournament
 
+# game record default directory
 DEFAULT_GAME_RECORD_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'game_records'))
 if not os.path.exists(DEFAULT_GAME_RECORD_DIRECTORY):
     os.makedirs(DEFAULT_GAME_RECORD_DIRECTORY)
@@ -14,68 +14,139 @@ if not os.path.exists(DEFAULT_GAME_RECORD_DIRECTORY):
 def get_game_record_path(filename):
     return os.path.join(DEFAULT_GAME_RECORD_DIRECTORY, filename)
 
+def analyze(game_records:list[caro.GameRecord]):
+
+    statistics, matches = tournament.analyze_game_record(game_records)
+
+    # sort by score
+    statistics.sort(key=lambda x: x['win'] + x['draw']*0.5, reverse=True)
+
+    # rank board
+    print("RANK BOARD(by score)")
+    print("-" * 102)
+    print(f"|{'Name':<50}{'Score':<10}{'win/draw/loss':<20}{'avg thinking time':<20}|")
+    print("-" * 102)
+    for s in statistics:
+        _name = s['name']
+        _score = s['win'] + s['draw']*0.5
+        _win_draw_loss = f'{s['win']}/{s['draw']}/{s['loss']}'
+        _avg_t = s['avg thinking time']
+        print(f"|{_name:<50}{_score:<10}{_win_draw_loss:<20}{_avg_t:<20.06f}|")
+    print("-" * 102)
+    print('\n\n')
+
+    # show matches
+    while True:
+        agent_name = input("Enter an agent name to see its matchups (or 'exit' to quit): ")
+        if agent_name == 'exit':
+            break
+        if agent_name not in matches:
+            print(f"Agent '{agent_name}' not found. Please try again.")
+            continue
+        print(f"\nHead-to-head results for agent '{agent_name}' against other agents:")
+        print("-" * 92)
+        print(f"|{'Opponent':<50}{'win/draw/loss':<20}{'Average moves/game':<20}|")
+        print("-" * 92)
+        for opponent, result in matches[agent_name].items():
+            _wdl = f'{result['win']}/{result['draw']}/{result['loss']}'
+            _avg_m = result['avg moves per game']
+            print(f"|{opponent:<50}{_wdl:<20}{_avg_m:<20.02f}|")
+        print("-" * 92)
+
+def menu():
+
+    print(f'''
+        1. Play vs AI
+           - Play a game against an AI agent using a graphical interface (built with Pygame).
+
+        2. Run Tournament
+           - Let a list of AI agents play a round-robin tournament.
+           - Game records will be saved to the default directory: '{DEFAULT_GAME_RECORD_DIRECTORY}'.
+           - Summary results will be printed to the terminal.
+
+        3. Analyze Game Records
+            - Load and analyze tournament results from game records saved in a file.
+              (File selected from the default directory: '{DEFAULT_GAME_RECORD_DIRECTORY}')
+            - Statistics will be printed to the terminal.
+            
+        4. Exit
+    ''')
+    while True:
+        try:
+            c = int(input(">> Your choice: "))
+            if 1 <= c <= 4:
+                return c
+            else:
+                print("Invalid choice! Please enter 1, 2, 3 or 4.")
+        except ValueError:
+            print("Invalid input! Please enter a number (1, 2, 3 or 4).")
+
+
 if __name__ == '__main__':
+    '''
+    -------------------------
+            AGENTS LIST
+    -------------------------
+    '''
+    agents = []
+    depth_range = [1, 2]
+    search_radius_range = [1, 2]
+    random_move_range = [0, 5]
+    for d in depth_range:
+        for sr in search_radius_range:
+            for rm in random_move_range:
+                agents.append(
+                    MiniMax(
+                        name=f"m_depth{d}_search_radius{sr}_random_move{rm}",
+                        evaluate_function=evaluate,
+                        depth=d, search_radius=sr,
+                        random_move=rm)
+                )
 
     '''
-    ------------------------------------
-                INITIALIZE
-    ------------------------------------
+    -------------------------
+        MAIN LOOP
+    -------------------------
     '''
-    # app
-    app = App(font_name='Roboto-Regular.ttf', unit_size=15)
+    while True:
+        choice = menu()
 
-    # player: Human & AI
-    human = caro.Human("Hu mần")
-    ai = MiniMax(
-        name="AI lỏ",
-        depth=2,
-        search_radius=1,
-        random_move=10,
-        evaluate_function=evaluate
-    )
+        # play vs AI
+        if choice == 1:
+            app = App(font_name='Roboto-Regular.ttf', unit_size=15)
+            app.play_n_game(
+                player_x=caro.Human("You"),
+                player_o=MiniMax(name="AI", evaluate_function=evaluate, depth=2, search_radius=1, random_move=10),
+                number_of_game=2,
+                first_turn=caro.X_PIECE
+            )
 
-    '''
-    ---------------------------------
-                PLAY
-    ---------------------------------
-    '''
-    number_of_game = 4
+        # run tournament
+        elif choice == 2:
+            print("Number of games each agent will play with every other agent:")
+            n = int(input(">> n = "))
 
-    # Human vs Human
-    # game_record = app.play_n_game(
-    #     player_x=human,
-    #     player_o=human,
-    #     number_of_game=number_of_game,
-    #     first_turn=caro.X_PIECE,
-    #     allow_take_back_move=True,
-    #     review_game_after_finish=True
-    # )
+            # make tournament
+            game_records = tournament.tournament(agents, n)
 
-    # AI vs AI
-    # game_record = app.play_n_game(
-    #     player_x=ai,
-    #     player_o=ai,
-    #     number_of_game=number_of_game,
-    #     first_turn=caro.X_PIECE,
-    #     allow_take_back_move=True,
-    #     review_game_after_finish=True
-    # )
+            # save result to file
+            path = get_game_record_path(f'tournament_{int(time.time())}.json')
+            caro.GameRecord.save_to_file(path, game_records)
+            print(f"Game record was saved to '{path}'")
 
-    # AI vs Human
-    game_record = app.play_n_game(
-        player_x=ai,
-        player_o=human,
-        number_of_game= number_of_game,
-        first_turn=caro.X_PIECE,
-        allow_take_back_move=True,
-        review_game_after_finish=True
-    )
+            # analyze
+            print('\n')
+            analyze(game_records)
 
-    '''
-    ---------------------------------
-        SAVE GAME RECORD
-    ---------------------------------
-    '''
-    file_name = f"game_record_{int(time.time())}.json"
-    caro.GameRecord.save_to_file(get_game_record_path(file_name), game_record)
-    print(f"Game record was saved to '{get_game_record_path(file_name)}'")
+        # analyze
+        elif choice == 3:
+            print(f"A file will be selected from the default directory: '{DEFAULT_GAME_RECORD_DIRECTORY}'")
+            file_name = input(">> File name: ")
+            game_records = caro.GameRecord.load_from_file(get_game_record_path(file_name))
+
+            print('\n\n')
+            analyze(game_records)
+
+        # exit
+        elif choice == 4:
+            break
--- src/tournament.py
+++ src/tournament.py
@@ -1,12 +1,53 @@
+from typing import Any
+import warnings
 import caro
 
 def play(agent_x:caro.AI, agent_o:caro.AI, first_turn:str)->caro.GameRecord:
-    pass
+    # check
+    if not (isinstance(agent_x, caro.AI) and isinstance(agent_o, caro.AI)):
+        raise RuntimeError("Agent must extend caro.AI")
+    if not (first_turn == caro.X_PIECE or first_turn == caro.O_PIECE):
+        raise RuntimeError("Invalid first turn")
+
+    player = {caro.X_PIECE:agent_x, caro.O_PIECE:agent_o}
+    current_turn = first_turn
+    board_state = caro.BoardState()
+    game_record = caro.GameRecord(player_x_name=agent_x.name, player_o_name=agent_o.name, first_turn=first_turn)
+    while board_state.status() == caro.NOT_FINISH:
+        move = player[current_turn].decide_move(board_state.clone(), current_turn)
+        board_state.put(current_turn, move)
+        game_record.add_move(move)
+
+        # change turn
+        current_turn = caro.X_PIECE if current_turn == caro.O_PIECE else caro.O_PIECE
+
+    return game_record
 
 def match(agent_x:caro.AI, agent_o:caro.AI, number_of_game:int)->list[caro.GameRecord]:
-    pass
+    # check
+    if not (isinstance(agent_x, caro.AI) and isinstance(agent_o, caro.AI)):
+        raise RuntimeError("Agent must extend caro.AI")
+    if number_of_game <= 0:
+        raise RuntimeError("Number of game must > 0")
 
-def tournament(agents:list[caro.AI], n_game:int=50)->list[caro.GameRecord]:
+    game_records = []
+    first_turn = caro.X_PIECE
+    print(f"{agent_x.name} vs {agent_o.name}")
+    for i in range(number_of_game):
+        print(f"\tgame {i+1}/{number_of_game}:", end=" ")
+        record = play(agent_x, agent_o, first_turn=first_turn)
+        game_records.append(record)
+
+        # change first turn after each game
+        first_turn = caro.X_PIECE if first_turn == caro.O_PIECE else caro.O_PIECE
+
+        result = record.result()
+        str_result = f"{agent_x.name} win" if result == caro.X_WIN else f"{agent_o.name} win" if result == caro.O_WIN else "draw"
+        print(f"{str_result} after {len(record.moves)} moves")
+
+    return game_records
+
+def tournament(agents:list[caro.AI], n_game:int=5)->list[caro.GameRecord]:
     """
      Run a round-robin tournament where each agent plays against every other agent.
     :param agents: list of agent
@@ -27,5 +68,120 @@ def tournament(agents:list[caro.AI], n_game:int=50)->list[caro.GameRecord]:
             game_records += match(agents[i], agents[j], n_game)
     return game_records
 
-def leader_board(game_records:list[caro.GameRecord]):
-    pass
+def analyze_game_record(game_records:list[caro.GameRecord]):
+    # check
+    for r in game_records:
+        if r.player_x == r.player_o:
+            raise RuntimeError(f"Invalid game record: Agent '{r.player_x}' cannot play against itself")
+    agent_names = set()
+    for r in game_records:
+        agent_names.add(r.player_x)
+        agent_names.add(r.player_o)
+    agent_names = list(agent_names)
+
+    name_to_id = {agent_names[i]:i for i in range(len(agent_names))}
+    statistics = []
+    move_count = [0 for _ in range(len(agent_names))] # use for calculate avg thinking time
+    for n in agent_names:
+        statistics.append({
+            "name": n,
+            "win":  0,
+            "loss": 0,
+            "draw": 0,
+            "avg thinking time": 0
+        })
+
+    matches = dict()
+    for i in range(len(agent_names)):
+        matches[agent_names[i]] = dict()
+        for j in range(len(agent_names)):
+            if i == j:
+                continue
+            matches[agent_names[i]][agent_names[j]] = {
+                "win": 0,
+                "loss": 0,
+                "draw": 0,
+                "avg moves per game": 0
+            }
+
+    def update_matches(agent1:str, agent2:str, result, n_moves):
+        # result agent1 vs agent2: win 1, draw 0, loss -1
+        n_game =  matches[agent1][agent2]['win'] + matches[agent1][agent2]['draw'] +matches[agent1][agent2]['loss']
+        old_avg  = matches[agent1][agent2]['avg moves per game']
+        matches[agent1][agent2]['avg moves per game'] = ((old_avg * n_game) + n_moves)/(n_game + 1)
+        if result == 1:
+            matches[agent1][agent2]['win'] += 1
+        elif result == -1:
+            matches[agent1][agent2]['loss'] += 1
+        elif result == 0:
+            matches[agent1][agent2]['draw'] += 1
+
+    def update_statistics(name, result, n_moves, total_thinking_time):
+        score_id = name_to_id[name]
+
+        # update avg time
+        old_n = move_count[score_id]
+        new_n = old_n + n_moves
+        old_avg = statistics[score_id]['avg thinking time']
+        statistics[score_id]['avg thinking time'] = ((old_avg*old_n)+total_thinking_time)/new_n
+        move_count[score_id] = new_n
+
+        # update result
+        if result == 1:
+            statistics[score_id]['win'] += 1
+        elif result == -1:
+            statistics[score_id]['loss'] += 1
+        elif result == 0:
+            statistics[score_id]['draw'] += 1
+
+    for r in game_records:
+        game_result = r.result()
+
+        n_move_0 = len(r.moves)//2
+        if (len(r.moves) %2) == 1:
+            n_move_0 += 1
+        n_move_1 = len(r.moves)//2
+        total_time_0 = sum([r.move_timestamps[i] - r.move_timestamps[i-1] for i in range(2, len(r.moves), 2)])
+        total_time_1 = sum([r.move_timestamps[i] - r.move_timestamps[i-1] for i in range(1, len(r.moves), 2)])
+
+        if r.first_turn == caro.X_PIECE:
+            x_n = n_move_0
+            o_n = n_move_1
+
+            x_t = total_time_0
+            o_t = total_time_1
+        else:
+            x_n = n_move_1
+            o_n = n_move_0
+
+            x_t = total_time_1
+            o_t = total_time_0
+
+        if game_result == caro.X_WIN:
+            # update matches
+            update_matches(r.player_x, r.player_o, 1, len(r.moves))
+            update_matches(r.player_o, r.player_x, -1, len(r.moves))
+
+            # update statistics
+            update_statistics(r.player_x, 1, x_n, x_t)
+            update_statistics(r.player_o, -1, o_n, o_t)
+        elif game_result == caro.O_WIN:
+            # update matches
+            update_matches(r.player_x, r.player_o, -1, len(r.moves))
+            update_matches(r.player_o, r.player_x, 1, len(r.moves))
+
+            # update statistics
+            update_statistics(r.player_x, -1, x_n, x_t)
+            update_statistics(r.player_o, 1, o_n, o_t)
+        elif game_result == caro.DRAW:
+            # update matches
+            update_matches(r.player_x, r.player_o, 0, len(r.moves))
+            update_matches(r.player_o, r.player_x, 0, len(r.moves))
+
+            # update statistics
+            update_statistics(r.player_x, 0, x_n, x_t)
+            update_statistics(r.player_o, 0, o_n, o_t)
+        elif game_result == caro.NOT_FINISH:
+            warnings.warn("There is an unfinished game in game records. This game will be ignored.")
+
+    return  statistics, matches
